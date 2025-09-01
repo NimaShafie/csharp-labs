ad4120d4bca024b4f3ee52636ecc00ee851fcb84
Nima Shafie <nimzshafie@gmail.com>
2025-09-01 13:13:44 -0700

    solution project structure complete with additional projects added

---
+++ Lab1_SmartTVRemoteControl/Lab1.SmartTvRemote.Domain/ITvEvents.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.SmartTvRemote.Domain
+{
+    public interface ITvEvents
+    {
+        event System.EventHandler<bool>? PowerChanged;
+        event System.EventHandler<int>? VolumeChanged;
+        event System.EventHandler<int>? ChannelChanged;
+    }
+}
