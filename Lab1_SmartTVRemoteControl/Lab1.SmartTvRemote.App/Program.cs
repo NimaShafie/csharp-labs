90ed59f0c91d681a7cbc457c5bc6936664b840af
Nima Shafie <nimzshafie@gmail.com>
2025-09-01 17:46:42 -0700

    creation of a fakeTv object to test some basic functionality

---
+++ Lab1_SmartTVRemoteControl/Lab1.SmartTvRemote.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using Lab1.SmartTvRemote.Domain;
+
+var tv = new FakeTv();
+
+// subscribe to events to see real-time output (like attaching a callback in C++)
+tv.PowerChanged += (_, on) => Console.WriteLine($"Power: {(on ? "On" : "Off")}");
+tv.VolumeChanged += (_, vol) => Console.WriteLine($"Volume: {vol}");
+tv.ChannelChanged += (_, ch) => Console.WriteLine($"Channel: {ch}");
+
+await tv.PowerOnAsync();
+await tv.SetVolumeAsync(tv.CurrentVolume + 1);
+await tv.ChangeChannelAsync(7);
--- Lab1_SmartTVRemoteControl/Lab1.SmartTvRemote.Domain/ITvEvents.cs
+++ Lab1_SmartTVRemoteControl/Lab1.SmartTvRemote.Domain/ITvEvents.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Lab1.SmartTvRemote.Domain
 {
+// events, (built-in observer) which raise only when states change
     public interface ITvEvents
     {
         event System.EventHandler<bool>? PowerChanged;
-        event System.EventHandler<int>? VolumeChanged;
-        event System.EventHandler<int>? ChannelChanged;
+        event System.EventHandler<int>?  VolumeChanged;
+        event System.EventHandler<int>?  ChannelChanged;
     }
 }
